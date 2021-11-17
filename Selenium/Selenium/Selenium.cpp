65d872013a4e0b2ed22f2f031b8587738a4cf60b
PPDimitrakova20 <PPDimitrakova20@codingburgas.bg>
2021-11-17 18:14:55 +0200

    Add foundation of menu. Close #3.

--- Selenium/Selenium/Selenium.cpp
+++ Selenium/Selenium/Selenium.cpp
@@ -1,6 +1,68 @@
 #include <iostream>
+#include <conio.h> //_kbhit, _getch
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //Declarations of the menu options
+    string menuOptions[3] = { "Play", "Rules", "Exit" };
+    int menuOptionsIndex = 0;
+
+    while (true)
+    {
+        //system("cls");
+
+        //Menu output
+        for (int i = 0; i < 3; i++)
+        {
+            if (menuOptionsIndex == i)
+            {
+                cout << "> " << menuOptions[i] << endl;
+            }
+            else
+            {
+                cout << "  " << menuOptions[i] << endl;
+            }
+        }
+
+        //Navigation
+        while (true)
+        {
+            if (_kbhit())
+            {
+                switch (_getch())
+                {
+                case 'w':
+                    menuOptionsIndex -= 1;
+                    if (menuOptionsIndex == -1)
+                    {
+                        menuOptionsIndex = 2;
+                    }
+                    system("cls");
+                    break;
+                case 's':
+                    menuOptionsIndex += 1;
+                    if (menuOptionsIndex == 3)
+                    {
+                        menuOptionsIndex = 0;
+                    }
+                    system("cls");
+                    break;
+                }
+
+                for (int i = 0; i < 3; i++)
+                {
+                    if (menuOptionsIndex == i)
+                    {
+                        cout << "> " << menuOptions[menuOptionsIndex] << endl;
+                    }
+                    else
+                    {
+                        cout << "  " << menuOptions[i] << endl;
+                    }
+                }
+            }
+        }
+    }
 }
