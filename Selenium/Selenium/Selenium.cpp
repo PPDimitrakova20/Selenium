4ef3cf0277969104cd7053be7a742ef8d92f26a8
GAIvanova20 <GAIvanova20@codingburgas.bg>
2021-11-27 13:29:52 +0200

    Update menu code

--- Selenium/Selenium/Selenium.cpp
+++ Selenium/Selenium/Selenium.cpp
@@ -17,6 +17,94 @@ void printLogo()
     cout << endl << endl;
 }
 
+// Menu outputs - the different options
+void firstOutput(string menu[3])
+{
+    int menuIndex = 0;
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (menuIndex == i)
+        {
+            cout << setw(38) << "> " << menu[i] << " <" << endl;
+        }
+        else
+        {
+            cout << setw(38) << " " << menu[i] << endl;
+        }
+    }
+}
+
+// Menu output changes after the certain option is selected
+void menuSelectedOption(string menu[3], int indexMenu)
+{
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (indexMenu == i)
+        {
+            cout << setw(38) << "> " << menu[indexMenu] << " <" << endl;
+        }
+        else
+        {
+            cout << setw(38) << "  " << menu[i] << "  " << endl;
+        }
+
+    }
+}
+int movementUp(int up)
+{
+    // This way we avoid the situation where it can go up forever
+    if (up <= -1)
+    {
+        return up = 2;
+    }
+    else
+    {
+        return up--;
+    }
+}
+int movementDown(int down)
+{
+    // This way we avoid the situation where it can go down forever
+    if (down >= 3)
+    {
+        return 0;
+    }
+    else
+    {
+        return down++;
+    }
+}
+
+void optionPicker(int selectedOption, bool play)
+{
+    // If the user is on one of the options
+    switch (selectedOption)
+    {
+        // Play
+    case 0:
+
+        displayExercise();
+        play = false;
+        break;
+
+        // Rules
+    case 1:
+
+        cout << "Rules: . . ." << endl;
+        cout << "Press ESC to go back to the menu" << endl;
+        break;
+
+        // Exit
+    case 2:
+
+        //FIXME: Doesn't end the program. I think it ends only the function
+        cout << "Thanks for playing!" << endl;
+        return;
+        break;
+    }
+}
 
 int main()
 {
@@ -27,18 +115,7 @@ int main()
 
     printLogo();
 
-        // Menu outputs - the different options
-    for (int i = 0; i < 3; i++)
-    {
-        if (menuOptionsIndex == i)
-        {
-            cout << setw(38) <<"> " << menuOptions[i] << " <"<< endl;
-        }
-        else
-        {
-            cout << setw(38) << " " << menuOptions[i] << endl;
-        }
-    }
+    firstOutput(menuOptions);
 
     cout << endl << "------------------------------------------------------------------------------";
 
@@ -51,95 +128,50 @@ int main()
         if (_kbhit())
         {
             system("cls");
-            
+
             switch (_getch())
             {
-           
-                // With W the user can go up the options
-                case 'w':
 
-                    menuOptionsIndex--;
-                    // This way we avoid the situation where it can go up forever
-                    if (menuOptionsIndex == -1)
-                    {
-                        menuOptionsIndex = 2;
-                    }
-                    break;
+                // With W the user can go up the options
+            case 'w':
+
+                //FIXME: The movement is bugged. It doesn't return the value.
+                menuOptionsIndex = movementUp(menuOptionsIndex);
+                break;
 
                 // With S the user can go down the options
-                case 's':
+            case 's':
 
-                    menuOptionsIndex++;
-                    // This way we avoid the situation where it can go down forever
-                    if (menuOptionsIndex == 3)
-                    {
-                        menuOptionsIndex = 0;
-                    }
-                    break;
+                //FIXME: The movement is bugged. It doesn't return the value.
+                menuOptionsIndex = movementDown(menuOptionsIndex);
+                break;
 
                 // If the user presses Enter
-                case 13:
+            case 13:
 
-                    flag = 1;
-                    break;
+                flag = 1;
+                break;
 
                 // If the user presses Escape to go back 
-                case 27:
+            case 27:
 
-                    flag = 0;
-                    break;
+                flag = 0;
+                break;
             }
 
-            // Menu output changes after the certain option is selected
             // This way we avoid the menu showing up when the user has already picked an option
             if (!flag)
             {
 
                 printLogo();
-
-                for (int i = 0; i < 3; i++)
-                {
-                    if (menuOptionsIndex == i)
-                    {
-                        cout << setw(38) << "> "<< menuOptions[menuOptionsIndex] << " <" << endl;
-                    }
-                    else
-                    {
-                        cout << setw(38) << "  " << menuOptions[i] << "  " << endl;
-                    }
-
-                }
-
+                menuSelectedOption(menuOptions, menuOptionsIndex);
                 cout << endl << "------------------------------------------------------------------------------";
 
             }
             else
             {
                 system("cls");
-                // If the user is on one of the options
-                switch (menuOptionsIndex)
-                {
-                    // Play
-                    case 0:
-
-                        displayExercise();
-                        play = false;
-                        break;
-
-                    // Rules
-                    case 1:
-
-                        cout << "Rules: . . ." << endl;
-                        cout << "Press ESC to go back to the menu" << endl;
-                        break;
-
-                    // Exit
-                    case 2:
-
-                        cout << "Thanks for playing!" << endl;
-                        return 0;
-                        break;
-                }
+                optionPicker(menuOptionsIndex, play);
             }
         }
     }
